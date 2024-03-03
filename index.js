c06994d6fc2b979fe8b1f8624b3197841b38d927
yash <yasharora2678@gmail.com>
2024-03-03 20:22:03 +0530

    Implemented Socket.io

--- index.js
+++ index.js
@@ -17,8 +17,40 @@ app.use('/api/chats', chatRoutes);
 app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
-app.listen(process.env.APP_PORT, function () {
+const server = app.listen(process.env.APP_PORT, function () {
   console.log(
     `Server is listening on port  ${process.env.APP_PORT}`.yellow.bold
   );
 });
+
+const io = require('socket.io')(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000"
+  }
+})
+
+io.on("connection", (socket) => {
+  console.log(`Connected to socket.io`);
+  
+  socket.on('setup', (userData) => {
+   socket.join(userData?._id);
+   socket.emit("connected")
+  })
+
+  socket.on('join chat', (room) => {
+    socket.join(room);
+    console.log("User Joined Room: "+room)
+  })
+
+  socket.on('new message', (newMessageRecieved) => {
+    let chat = newMessageRecieved.chat
+    if(!chat.users) return console.log("chat.users is not defined");
+
+    chat.users.forEach((user)=>{
+      if(user._id == newMessageRecieved.sender._id) return;
+  
+      socket.in(user._id).emit("message recieved", newMessageRecieved)
+    })
+  });
+})
